71da752e86e4f48add19fbafe707719f934b4ec4
stevenDeveloperSvc <148901133+stevHernandez28@users.noreply.github.com>
2024-01-25 12:05:37 -0400

    fix: reubicacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { TopInfoComponent } from './home/hero/top-info/top-info.component';
+import { TopInfoComponent } from './nav-bar/top-info/top-info.component';
 import { HeroComponent } from './home/hero/hero.component';
 import { SearchPropertyComponent } from './home/hero/search-property/search-property.component';
 import { CategoriesComponent } from './home/categories/categories.component';
