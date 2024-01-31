df17411d675bcfa199a5999185588b68c7ce1212
stevenDeveloperSvc <134965137+stevenDeveloperSvc@users.noreply.github.com>
2024-01-31 09:22:23 -0400

    fix: estillos finales para entrega de diseno

---
+++ src/app/Routes/Route.routing.ts
@@ -0,0 +1,18 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "../Components/home/home.component";
+import { AboutUsComponent } from "../Components/about-us/about-us.component";
+import { AgentDetailsComponent } from "../Components/agent-details/agent-details.component";
+import { ContactMeComponent } from "../Components/contact-me/contact-me.component";
+import { FAQComponent } from "../Components/faq/faq.component";
+import { PropertyComponent } from "../Components/property/property.component";
+
+export const routes : Routes=[
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutUsComponent },
+    {path: 'property',component: PropertyComponent},
+    { path: 'agentdetails', component: AgentDetailsComponent },
+    { path: 'contact', component: ContactMeComponent },
+    { path: 'faq', component: FAQComponent },
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
