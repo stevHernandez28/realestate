14125a2f857146285e10dd6fb953a20444a3dd3c
stevenDeveloperSvc <134965137+stevenDeveloperSvc@users.noreply.github.com>
2024-03-04 17:26:10 -0400

    feat: convertir datos en un array de objetos

--- src/app/Components/property/property-list/property-list-item/property-list-item.component.ts
+++ src/app/Components/property/property-list/property-list-item/property-list-item.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class PropertyListItemComponent {
 
+    data = [{
+        header:{
+          image:'./assets/house.jpg',
+          title:'FOR RENT',
+          price:'$34,900',
+          priceRate: 'Month'
+        },
+        body:{
+          title:'New Apartment Nice View',
+          address: 'Belmont Gardens, Chicago',
+          details: '3 Bed &nbsp; 2 Bath &nbsp; 3450 Square Ft'
+        },
+        footer:{
+          autor: './assets/author.jpg',
+          agentName : ''
+        }
+    }]
 }
